7dd0f9beea32dacae70e05820ab07bf85da1cf5b
lesterprem <45570905+lesterprem@users.noreply.github.com>
2023-11-23 00:32:03 +0100

    pass all changes in practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(testArray,searchNum) {
+indexToRemove= testArray.indexOf(searchNum);
+return testArray.splice(indexToRemove,1);
 };
 
 // Do not edit below this line
