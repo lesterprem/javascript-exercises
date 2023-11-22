ab7b8f702b81508ef8ff9bae9d77cc9398c149b8
lesterprem <45570905+lesterprem@users.noreply.github.com>
2023-11-23 00:30:20 +0100

    exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+if (num<0) {
+    return ('ERROR');
+}
+else {
+    let y ="";
+    for (i=0;i< num; i++){
+        y+=string;
+    }
+return(y);    
+}
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
