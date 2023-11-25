5d4e95b33f2e9a90ea2022c6224ddfe5cd20c63f
lesterprem <45570905+lesterprem@users.noreply.github.com>
2023-11-25 18:38:09 +0100

    completed palindrome test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+const checkedword= word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return checkedword.split("").reverse().join("")===checkedword;
 };
 
 // Do not edit below this line
