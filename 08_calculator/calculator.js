9cee6e9e719eeefd364e89dca10fbf43cb939b00
lesterprem <45570905+lesterprem@users.noreply.github.com>
2023-11-25 18:06:57 +0100

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,current) => total+current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((product, current) => product * current,1) ;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a<0){return "ERROR"};
+  if(a===0){return 1};
+  let sum=0;
+  for (let i=1;i<=a;i++){
+    sum+=i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
